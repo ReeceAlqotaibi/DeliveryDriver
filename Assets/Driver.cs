62df200898a8560a03b1a86494180c7d2374af24
Reece Alqotaibi <reecea@cyclops-electronics.com>
2021-12-17 07:45:46 -0800

    Adjusted turning - car now only turns when moving. Also added border around the track so the car cannot go out of the scene.

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField] public float steerSpeed = 225f;
+    [SerializeField] private float steerSpeed = 225f;
     [SerializeField] public float originalMoveSpeed = 7.5f;
     [SerializeField] public float modifiedMoveSpeed;
     public bool hasNormalSpeed = true;
     public bool hasSpeedIncrease = false;
     public bool hasSpeedDecrease = false;
 
+    private float oldPositionX;
+    private float oldPositionY;
+
+
+
 
     void Start()
     {
-
+        oldPositionX = transform.position.x;
+        oldPositionY = transform.position.y;
     }
 
 
@@ -23,9 +29,15 @@ public class Driver : MonoBehaviour
         float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
         float moveAmount = Input.GetAxis("Vertical") * modifiedMoveSpeed * Time.deltaTime;
 
-        // Rotates on the {x} {y} {z} axis.
-        transform.Rotate(0, 0, -steerAmount);
+        if((oldPositionY < transform.position.y) || (oldPositionY > transform.position.y) || (oldPositionX > transform.position.x) || (oldPositionX < transform.position.x))
+        {
+            // Rotates on the {x} {y} {z} axis.
+            transform.Rotate(0, 0, -steerAmount);
+        }
         
+        oldPositionX = transform.position.x;
+        oldPositionY = transform.position.y;
+
         //Moves on {x} {y} {z} axis.
         transform.Translate(0, moveAmount, 0);
     }
