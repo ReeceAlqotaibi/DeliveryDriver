823f1f956d418df0f9c5ca555e501ddd1587b234
Reece Alqotaibi <reecea@cyclops-electronics.com>
2021-12-16 17:52:23 -0800

    Added speed increase and decrease pads with logic.

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
-    bool hasPackage = false;
+
+    //bool hasPackage = false;
     void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Hit something!");
@@ -12,6 +13,9 @@ public class Delivery : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
+        GameObject BlueCar = GameObject.Find("BlueCar");
+        Driver driver = BlueCar.GetComponent<Driver>();
+
         if (other.tag == "Package")
         {
             Debug.Log("Picked up package!");
@@ -21,5 +25,37 @@ public class Delivery : MonoBehaviour
         {
             Debug.Log("Picked up a customer!");
         }
+        
+        // Speed boost at normal speed
+        if(other.tag == "SpeedIncrease" && driver.hasNormalSpeed == true)
+        {
+            driver.hasSpeedIncrease = true;
+            driver.hasNormalSpeed = false;
+            driver.modifiedMoveSpeed = driver.originalMoveSpeed * 1.5f;
+        }
+        // Speed boost at decreased speed
+        else if(other.tag == "SpeedIncrease" && driver.hasSpeedDecrease == true)
+        {
+            driver.hasNormalSpeed = true;
+            driver.hasSpeedDecrease = false;
+            driver.modifiedMoveSpeed = driver.originalMoveSpeed;
+        }
+        
+        // Speed decrease at normal speed
+        if(other.tag == "SpeedDecrease" && driver.hasNormalSpeed == true)
+        {
+            driver.hasNormalSpeed = false;
+            driver.hasSpeedDecrease = true;
+            driver.modifiedMoveSpeed = driver.originalMoveSpeed / 1.5f;
+        }
+        // Speed decrease at boosted speed
+        else if(other.tag == "SpeedDecrease" && driver.hasSpeedIncrease == true)
+        {
+            driver.hasNormalSpeed = true;
+            driver.hasSpeedDecrease = false;
+            driver.hasSpeedIncrease = false;
+            driver.modifiedMoveSpeed = driver.originalMoveSpeed;
+        }
     }
+
 }
--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField] float steerSpeed = 3f;
-    [SerializeField] float moveSpeed = 0.1f;
+    [SerializeField] public float steerSpeed = 225f;
+    [SerializeField] public float originalMoveSpeed = 7.5f;
+    [SerializeField] public float modifiedMoveSpeed;
+    public bool hasNormalSpeed = true;
+    public bool hasSpeedIncrease = false;
+    public bool hasSpeedDecrease = false;
 
 
     void Start()
@@ -17,7 +21,7 @@ public class Driver : MonoBehaviour
     void Update()
     {   
         float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
-        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * modifiedMoveSpeed * Time.deltaTime;
 
         // Rotates on the {x} {y} {z} axis.
         transform.Rotate(0, 0, -steerAmount);
@@ -25,4 +29,5 @@ public class Driver : MonoBehaviour
         //Moves on {x} {y} {z} axis.
         transform.Translate(0, moveAmount, 0);
     }
+       
 }
